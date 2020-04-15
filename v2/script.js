532e3e1067d0796cf1cbfbb721cc42e111fe4186
Paul <tagger@list.ru>
2020-04-15 20:19:04 +0300

    Это наш первый коммит

---
+++ v2/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+// var number =5;
+// var string = 'hello';
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log
+// let persone = {
+//     name: "john",
+//     age: 25,
+//     ismarried: false
+// }
+// console.log(persone["name"]);
+
+// alert('hello');
+
+let answer =prompt("joppa?", 'da');
+
+console.log(typeof(answer));
